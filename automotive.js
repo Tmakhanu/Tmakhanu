d91b95eb5a1e4b3ad44bce464da58a6d2a89c6f2
Tmakhanu <teclamakhanu@gmail.com>
2022-05-18 20:17:20 +0300

    Tecla's description

--- automotive.js
+++ automotive.js
@@ -4,11 +4,13 @@
 <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Git</title>
+    <title>Tecla Makhanu</title>
 </head>
 <body>
-Git is an Open Source Distributed Version Control System. Now that's a lot of words to define Git. 
-Let me break it down and explain the wording: Control System:
- This basically means that Git is a content tracker. 
+I have always been fascinated from a young age, by the power of technology and its evolvement in all of the
+world's dominant sectors, from health research discoveries to information communication. I therefore took an
+initiative to deepen my knowledge in information technology.
+With my interest and expertise, I am confident that am able to come up with innovative solutions to some of my
+community's everyday problems. 
 </body>
 </html>
