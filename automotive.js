cdcc817b7e0729dedabb6d15ae840a3c1c03084f
Tmakhanu <teclamakhanu@gmail.com>
2022-05-18 20:03:57 +0300

    git course first commit

---
+++ automotive.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Git</title>
+</head>
+<body>
+Git is an Open Source Distributed Version Control System. Now that's a lot of words to define Git. 
+Let me break it down and explain the wording: Control System:
+ This basically means that Git is a content tracker. 
+</body>
+</html>
